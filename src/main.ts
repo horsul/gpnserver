64fb3e047dc30baf578c3d0fd7e999a4baa0a764
horsul <horsul@yandex.ru>
2022-02-28 23:00:45 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = app.get(ConfigService);
 
   const port = config.get<number>('PORT') || 3000;
